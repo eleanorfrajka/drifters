badbcd8262e9d9da37fcd24348a511b1134220f5
Eleanor Frajka-Williams <eleanorfrajka@gmail.com>
2022-01-11 10:49:04 +0000

    Updated some filenames to be used in scripts

--- 02-code/SIO_wrap/fnames.py
+++ 02-code/SIO_wrap/fnames.py
@@ -1,3 +1,6 @@
 fname_rawdata = 'drifter_data_raw_'
 fname_data = 'drifter_data_'
 fname_data = "sio_"
+project_name = 'TERIFIC'
+operator_name = "EFW"
+institution_name = 'National Oceanography Centre, UK'
